d787333ba0e35eb5d2ee88059c22bb7533b95771
cecil <ola@example.com>
2022-07-06 19:01:56 +0200

    install and config babel and webpack

---
+++ src/components/QuestionCard.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+type Props = {}
+function QuestionCard() {
+    return (
+        <div>
+            Question Card
+        </div>
+    )
+}
+
+export default QuestionCard;
